0e6d783c18aba1280cc8ca7de0d3884927b8b9eb
Sai Rahul Urumu <123154452+usrahul1@users.noreply.github.com>
2024-06-08 11:46:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector("#menu-icon")
+let nav = document.querySelector(".navlist")
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x")
+    nav.classList.toggle("open")
+}
